6eca6bef0b5a02ba08fd8698622900cae680c2f2
alcanceioficial-crypto <alcancei.oficial@gmail.com>
2025-12-15 11:45:43 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const healthRoutes = require('./routes/health.routes');
+const pixRoutes = require('./routes/pix.routes');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/health', healthRoutes);
+app.use('/pix', pixRoutes);
 
 module.exports = app;
 
