3748a1587a5205c61838f0957a14975e3b00687a
alcanceioficial-crypto <alcancei.oficial@gmail.com>
2025-12-19 16:01:26 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
+
 const app = require('./app');
-require('./utils/ensureCert'); // mantém isso, já está funcionando
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-  console.log('🚀 Servidor rodando na porta', PORT);
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
