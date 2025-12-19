988855616663c003d5b124bd7b423a484d301abb
alcanceioficial-crypto <alcancei.oficial@gmail.com>
2025-12-19 09:56:07 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
+
 const app = require('./app');
+const ensureCert = require('./utils/ensureCert');
+const pixRoutes = require('./routes/pix.routes');
+
+ensureCert(); // 🔐 SEM ISSO NADA FUNCIONA
+
+app.use('/api', pixRoutes);
 
 const PORT = process.env.PORT || 3333;
-
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
