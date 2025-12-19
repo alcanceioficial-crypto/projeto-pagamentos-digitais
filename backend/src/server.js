04291c713bca3b13cace1168e482b8a7e3c227a0
alcanceioficial-crypto <alcancei.oficial@gmail.com>
2025-12-19 16:26:34 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,11 @@
 const app = require('./app');
 
+const pixRoutes = require('./routes/pix.routes');
+const pixWebhookRoutes = require('./routes/pixWebhook.routes');
+
+app.use('/api/pix', pixRoutes);
+app.use('/api/webhook', pixWebhookRoutes);
+
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
