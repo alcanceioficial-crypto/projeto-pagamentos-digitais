169a4b05aa0e93d1adb120d0a2091425bf50a353
alcanceioficial-crypto <alcancei.oficial@gmail.com>
2025-12-19 14:10:38 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,18 @@ const express = require('express');
 const cors = require('cors');
 
 const pixRoutes = require('./routes/pix.routes');
-const pixWebhookRoutes = require('./routes/pixWebhook.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/pix', pixRoutes);
-app.use('/pix', pixWebhookRoutes);
+// 👇 ESSA LINHA É O QUE ESTAVA FALTANDO
+app.use('/api/pix', pixRoutes);
+
+// rota simples pra teste
+app.get('/', (req, res) => {
+  res.send('API PIX EFÍ OK');
+});
 
 module.exports = app;
