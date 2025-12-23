860f2acb2edfb6202521f5d2830e787a92f9055c
alcanceioficial-crypto <alcancei.oficial@gmail.com>
2025-12-23 15:50:19 -0300

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,15 +1,8 @@
 const { Pool } = require('pg');
 
-if (!process.env.DATABASE_URL) {
-  console.error('❌ DATABASE_URL não definida');
-  process.exit(1);
-}
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
